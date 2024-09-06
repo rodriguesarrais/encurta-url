928865d764ee4f7f7cdea614a4d63f97c4c849c4
rodriguesarrais <rodriguesarrais10@gmail.com>
2024-09-06 10:18:41 -0300

    initial commit

---
+++ src/main/java/dev/rodriguesarrais/encurta_url/EncurtaUrlApplication.java
@@ -0,0 +1,13 @@
+package dev.rodriguesarrais.encurta_url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rodriguesarrais/encurta_url/EncurtaUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rodriguesarrais.encurta_url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
